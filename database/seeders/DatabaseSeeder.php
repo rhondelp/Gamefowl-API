948bec40c286e5f86fbf099204c24e986eccdd49
NMSCSTR <rhondel.pagobo@nmsc.edu.ph>
2026-09-26 23:26:32 +0800

    refactor: adding use Illuminate\Database\Console\Seeds\WithoutModelEvents in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use App\Models\User;
 
 /**
  * File: database/seeders/DatabaseSeeder.php
